9319509c05cc9569dbd06fe8c0bfbd06487a72da
Kalgrand <micdeg96@gmail.com>
2019-05-24 17:53:04 +0200

    snake

--- snake.js
+++ snake.js
@@ -5,30 +5,29 @@ window.onload = function () {
     const snakeW = 20;
     const snakeH = 20;
 
-    function drawSnake(x, y) {
+    function Snake(x, y) {
         ctx.fillStyle = "#FFDEAD";
         ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
     }
 
-    var len = 2;
-    var snake = [];
+    function Draw() {
+        var length = 2;
+        var snake = [];
 
-    for (var i=len-1; i>=0; i--){
-        snake.push(
-            {
-                x : i,
-                y : 0
-            }
-        );
-    }
-
-    function draw() {
-        for (var i = 0; i < snake.length; i++) {
-            var x = snake[i].x;
-            var y = snake[i].y;
-            drawSnake(x, y);
+        for (var i=length-1; i>=0; i--){
+            snake.push(
+                {
+                    x : i,
+                    y : 0
+                }
+            );
+        }
+        for (var j = 0; j < snake.length; j++) {
+            var x = snake[j].x;
+            var y = snake[j].y;
+            Snake(x, y);
         }
     }
-    draw();
+    Draw();
 
 }
