2ccb73b9f1836ce0004b50f5a8b872f67bdd5c39
Kalgrand <micdeg96@gmail.com>
2019-05-26 14:57:37 +0200

    snake

--- snake.js
+++ snake.js
@@ -5,7 +5,7 @@ window.onload = function () {
     const snakeW = 20;
     const snakeH = 20;
 
-    var score = 0;
+    //var score = 0;
     var control = "RIGHT";
 
     document.addEventListener("keydown",direction);
@@ -22,15 +22,15 @@ window.onload = function () {
         }
     }
 
-    function Score(x) {
-        ctx.fillStyle = "black";
-        ctx.font = '15px serif';
-        ctx.fillText("SCORE : " + x, 5 , cvs.height - 10 );
-    }
-    
+   // function Score(x) {
+   //     ctx.fillStyle = "black";
+   //     ctx.font = '15px serif';
+   //     ctx.fillText("SCORE : " + x, 5 , cvs.height - 10 );
+   // }
+
     function Snake(x, y) {
         //ctx.fillStyle = "#FFDEAD";
-        var my_gradient = ctx.createLinearGradient(0, 250, 0, 200);
+        var my_gradient = ctx.createLinearGradient(0,0, 310,310);
         my_gradient.addColorStop(1, "Khaki");
         my_gradient.addColorStop(0.5, "DarkOrange");
         my_gradient.addColorStop(1, "SandyBrown");
@@ -54,9 +54,64 @@ window.onload = function () {
         x : Math.round(Math.random()*(1+6+5)),
         y : Math.round(Math.random()*(5+8+4))
     }
-    
+
+    var stone = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone2 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone3 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone4 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone5 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone6 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone7 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone8 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone9 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
+    var stone10 = {
+        x : Math.round(Math.random()*(Math.PI * 8)),
+        y : Math.round(Math.random()*(Math.PI * 8))
+    }
+
     function Food(x,y) {
         ctx.fillStyle = "red";
+        ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
+    }
+
+    function Stone(x,y) {
+        ctx.fillStyle = "silver";
         ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
     }
 
@@ -70,6 +125,16 @@ window.onload = function () {
         }
 
         Food(food.x,food.y);
+        Stone(stone.x,stone.y);
+        Stone(stone2.x,stone2.y);
+        Stone(stone3.x,stone3.y);
+        Stone(stone4.x,stone4.y);
+        Stone(stone5.x,stone5.y);
+        Stone(stone6.x,stone6.y);
+        Stone(stone7.x,stone7.y);
+        Stone(stone8.x,stone8.y);
+        Stone(stone9.x,stone9.y);
+        Stone(stone10.x,stone10.y);
 
         var snakeX = snake[0].x;
         var snakeY = snake[0].y;
@@ -84,7 +149,7 @@ window.onload = function () {
         if (control == "DOWN") snakeY++;
 
         if(snakeX == food.x && snakeY == food.y){
-            score++;
+            //score++;
             food = {
                 x : Math.round(Math.random()*(1+6+5)),
                 y : Math.round(Math.random()*(5+8+4))
@@ -93,13 +158,25 @@ window.onload = function () {
             snake.pop();
         }
 
+        if(snakeX == stone.x && snakeY == stone.y || snakeX == stone2.x && snakeY == stone2.y || snakeX == stone3.x && snakeY == stone3.y
+            || snakeX == stone4.x && snakeY == stone4.y || snakeX == stone5.x && snakeY == stone5.y || snakeX == stone6.x && snakeY == stone6.y
+            || snakeX == stone7.x && snakeY == stone7.y || snakeX == stone8.x && snakeY == stone8.y || snakeX == stone9.x && snakeY == stone9.y
+            || snakeX == stone10.x && snakeY == stone10.y){
+            //score++;
+            stone = {
+                x : Math.round(Math.random()*(Math.PI * 8)),
+                y : Math.round(Math.random()*(Math.PI * 8))
+            }
+            location.reload();
+        }
+
         var newHead = {
             x: snakeX,
             y: snakeY
         }
 
         snake.unshift(newHead);
-        Score(score);
+        //Score(score);
     }
     setInterval(Draw,60);
 }
